cd6845da4ffd738c01d456c3e531c49a6c641ad3
arjunyuvaraj <arjunycoding@gmail.com>
2021-12-18 15:39:09 -0500

    adding files about numbers and varibles

---
+++ dataTypes/numbers.py
@@ -0,0 +1,41 @@
+#*************#
+# INT A FLOAT #
+#*************#
+# int & float
+# int   --> intergers -1,0,1,2,3 ect.
+# float --> decimal numbers --> 1.1, 1.2, 1.3 ect.
+
+#************#
+# OPERATIONS #
+#************#
+print(2 + 4) # Add
+print(2 - 4) # Subtract
+print(2 * 4) # Mutiply
+print(2 / 4) # Divid
+print(2 ** 4) # Exponnt
+print(2 // 4) # Floor
+print(2 % 4) # Modulo(remanider)
+
+
+#*********#
+# TYPE OF #
+#*********#
+
+print(type(3 + 9))
+# 1. evaluate what's inside the type function
+# print(type(12))
+# 2. find type of the number 12 --> int
+
+
+#****************#
+# MATH FUNCTIONS #
+#****************#
+print(round(3.1))
+print(abs(-3.1))
+# MORE FUNCTIONS https://www.programiz.com/python-programming/modules/math
+
+#**********************************#
+# EXTRA! EXTRA! READ ALL ABOUT IT! #
+#**********************************#
+# type of 0 is 'int' or 'intergers'
+# type(9.9 + 1.1) --> float. computer says 11.0.
